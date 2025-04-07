bfc4f8c32b64ab46829bf6f2ebbba82f39e20c74
ieatmotherboards <hendres7373@gmail.com>
2025-04-07 19:28:48 -0400

    implement basic login logic

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 '''
 To use this: just run main.py in the 312_project server and control click the link that it prints to the console
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route("/login_data", methods=["POST"])
+def parse_data():
+    data = request.get_json()
+    print("got data")
+    js = jsonify({'username': data["username"], "password":data["password"]})
+    print(f"got username:{data["username"]} and password:{data["password"]}")
+    return js
 
 if __name__ == '__main__':
     app.run(debug = True)
