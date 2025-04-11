9c1427b6f11750a031387e3ec75eb33bf9a91477
ieatmotherboards <hendres7373@gmail.com>
2025-04-11 10:34:23 -0400

    added HTML pages and basic logging

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 To use this: just run main.py in the 312_project server and control click the link that it prints to the console
 '''
 from flask import Flask, render_template, request, jsonify
+from auth import parse_data
+
+import logging
+
+logging.basicConfig(filename='record.log', level=logging.INFO, filemode="w")
+
 
 app = Flask(__name__)
 
@@ -14,17 +20,16 @@ def login():
     return render_template("login.html")
 
 @app.route("/login_data", methods=["POST"])
-def parse_data():
-    data = request.get_json()
-    print("got data")
-    js = jsonify({'username': data["username"], "password":data["password"]})
-    print(f"got username:{data["username"]} and password:{data["password"]}")
-    return js
+def parse_login():
+    return parse_data()
+
+@app.route('/casino')
+def render_casino():
+    return render_template("casino.html")
+
+@app.route('/mines')
+def render_mines():
+    return render_template("mines.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-'''
-Where to go from here:
-    - 
-'''
