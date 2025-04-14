6f454d7c387a2d90e985466b4f09772def02be8b
Superice0 <etsnyder@buffalo.edu>
2025-04-14 16:05:18 -0400

    test ethan branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
 To use this: just run main.py in the 312_project server and control click the link that it prints to the console
 '''
+#hello world
 from flask import *
 from auth import parse_data, register_new_account
 
