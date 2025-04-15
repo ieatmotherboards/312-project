e9954c0a924ccf0e6bb1a4c111c68faa65234c99
domom <68618855+domomTV@users.noreply.github.com>
2025-04-15 15:24:08 -0400

    updated mines game
    - added clickable coal functionality
    - removed old casino & mines html
    - added a test for the database
    - updated .gitignore to exclude each loathsome __pycache__ folder that may find its way into our project

--- phaser-game/src/mines/scenes/Game.js
+++ phaser-game/src/mines/scenes/Game.js
@@ -1,5 +1,3 @@
-import { Player } from '../../gameObjects/Player.js';
-
 export class Game extends Phaser.Scene {
     constructor() {
         super('Game');
@@ -9,7 +7,22 @@ export class Game extends Phaser.Scene {
     create() {
         this.add.image(400, 300, 'cave');
 
-        this.coal = this.add.image(400, 300, 'coal');
+        this.coal = this.add.sprite(400, 300, 'coal').setInteractive().setScale(4, 4);
+        this.breaking = false
+
+        this.coal.on('pointerdown', function (pointer) {
+            if (!this.breaking) {
+                this.setTexture('coal_breaking');
+                this.breaking = true;
+            } else {
+                this.visible = false;
+                this.coin = this.scene.physics.add.sprite(this.x, this.y, 'coin');
+                this.coin.setScale(2.5, 2.5);
+                this.coin.setGravityY(500);
+                this.coin.setBounceY(0.5);
+                this.coin.setCollideWorldBounds(true);
+            }
+        });
         
         this.cursors = this.input.keyboard.createCursorKeys();
 
