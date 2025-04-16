2f059cbf7d57859b60c235cf80e4f9ce38eecf54
Superice0 <etsnyder@buffalo.edu>
2025-04-16 13:45:38 -0400

    File for slots with ideas for symbols. will continue to develope throught the day

---
+++ phaser-game/GameCode/Slots.py
@@ -0,0 +1,25 @@
+
+
+'''
+Symbol Ideas For buying new symbols
+Jesse
+Stack Over Flow
+Mason Lary
+Windows
+Linux
+chatGpt (Ai violation)
+Kris Schindler
+Alan Hunt Drop Tables
+
+Normal Slots symbols
+
+Cherries
+Lemon
+BAR
+Bell
+Watermelon
+Seven (7)
+Plum
+Star
+
+'''
